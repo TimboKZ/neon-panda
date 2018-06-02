d6c4377dbf70c5151e995ba7baa0251842883aca
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2018-06-02 16:48:46 -0700

    Optimize page speed

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,27 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('compress', function (cb) {
+    gulp.src('assets/js/index.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/js'));
+    gulp.src('assets/css/style.css')
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/css'));
+    gulp.src('assets/js/prism/prism.css')
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/js/prism'));
 });
 
 gulp.task('watch', function () {
